4621a8792bb944242a9e43a11764607220b71a2e
jciglesias <jiglesia@student.42.fr>
2022-02-20 11:56:02 +0100

    makefile

---
+++ philo/main.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiglesia <jiglesia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 18:53:17 by jiglesia          #+#    #+#             */
+/*   Updated: 2022/02/19 12:17:05 by jiglesia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	lyceum(int *dir)
+{
+	(void)dir;
+}
+
+int	ft_isdigit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	if (str[i] == '-')
+		i++;
+	n = 0;
+	while (str[i])
+	{
+		n = n * 10 + (str[i++] - 48);
+	}
+	//missing sign
+	return (n);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	dir[5];
+
+	if (argc == 5 || argc == 6)
+	{
+		i = 0;
+		while (++i < argc)
+		{
+			if (!ft_isdigit(argv[i]))
+			{
+				printf("%s wrong value", argv[i]);
+				return (0);
+			}
+			dir[i -1] = ft_atoi(argv[i]);
+		}
+		lyceum(dir);
+		return (0);
+	}
+	printf("ERROR: expected:./philo [#of philosophers] ");
+	printf("[time to die] [time to eat] [time to sleep]\n");
+	return (0);
+}
